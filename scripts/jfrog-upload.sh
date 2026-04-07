9dd630194cc92e46371ae9cb0d1bab4f9d574d8d
Poushali Chattopadhyay <poushali.chattopadhyay11@gmail.com>
2026-04-08 00:09:19 +0530

    Update jfrog-upload.sh

--- scripts/jfrog-upload.sh
+++ scripts/jfrog-upload.sh
@@ -1,1 +1,12 @@
-
+#!/bin/bash
+echo "Setting up JFrog CLI..."
+curl -fL https://getcli.jfrog.io | sh
+# Configure using environment variables
+./jfrog config add my-server \
+  --url=$JFROG_URL \
+  --access-token=$JFROG_TOKEN \
+  --interactive=false
+echo "Uploading artifact to Artifactory..."
+./jfrog rt upload "app/*" demo-repo/
+echo "Triggering Xray scan..."
+./jfrog xr scan demo-repo/
